6ac8e67dc6c2a342cb8c85bcd237446d1ab4dd9c
relaxrazlom <relaxrazlom@mail.ru>
2023-08-31 16:10:15 +0300

    deploy foodgram

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -181,6 +181,11 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
         self._create_ingredients(instance, ingredients)
         tags = self.initial_data.get('tags')
         instance.tags.set(tags)
+        instance.image = validated_data.get('image', instance.image)
+        instance.name = validated_data.get('name', instance.name)
+        instance.text = validated_data.get('text', instance.text)
+        instance.cooking_time = validated_data.get(
+            'cooking_time', instance.cooking_time)
         instance.save()
         return instance
 
