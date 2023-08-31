b044148cc8b265e2a5b3505c188afa882731eec1
relaxrazlom <relaxrazlom@mail.ru>
2023-08-31 11:22:39 +0300

    deploy foodgram

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -96,7 +96,7 @@ USE_L10N = True
 USE_TZ = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/media'
 
 STATIC_URL = '/staticfiles/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
