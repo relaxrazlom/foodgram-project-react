4c3aea6994d1926226fc182aa5ebec034d6a2478
relaxrazlom <relaxrazlom@mail.ru>
2023-08-31 10:30:25 +0300

    deploy foodgram

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -98,8 +98,8 @@ USE_TZ = True
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/staticfiles/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
